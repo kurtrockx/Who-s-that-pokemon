72927a7f7cd82d24aa3e34874798fb0c2fb4092a
derpfear <kurtdebelen431@gmail.com>
2025-03-26 18:53:42 +0800

    added click for cry indicator on pokemon image hover

--- src/PokemonDisplay.jsx
+++ src/PokemonDisplay.jsx
@@ -26,12 +26,17 @@ export function PokemonDisplay({ chosenPokemon }) {
     <Loader />
   ) : (
     <div className="mx-auto flex h-full items-center gap-2 duration-500 starting:scale-20">
-      <img
-        src={pokemonData.sprites.front_default}
-        alt="pokemon_image"
-        className="min-h-36 cursor-pointer origin-right delay-500 starting:scale-x-0 duration-200 hover:scale-105 active:scale-90"
-        onClick={handleCry}
-      />
+      <div className="group relative h-full origin-right overflow-hidden delay-500 duration-200 starting:scale-x-0">
+        <img
+          src={pokemonData.sprites.front_default}
+          alt="pokemon_image"
+          className="min-h-36 cursor-pointer duration-100 hover:scale-105 active:scale-90"
+          onClick={handleCry}
+        />
+        <p className="pointer-events-none absolute bottom-4 left-0 origin-left ease-in-out -translate-x-100 rounded-2xl border-2 bg-black px-2 text-xs text-white delay-100 duration-200 group-hover:translate-0">
+          click for cry
+        </p>
+      </div>
       <PokemonStats stats={pokemonData.stats} />
       <audio src={pokemonData.cries.latest} ref={pokemonCry}></audio>
     </div>
--- src/ProgressBar.jsx
+++ src/ProgressBar.jsx
@@ -3,13 +3,13 @@ export function ProgressBar({ level, endLevel, points }) {
     <div>
       <div className="flex justify-between">
         <p className="font-bold text-white">
-          score: <span className="text-primary">{points}</span>
-        </p>
-        <p className="font-bold text-white">
           level:{" "}
           <span className="text-primary">
             {level}/{endLevel}
           </span>
+        </p>
+        <p className="font-bold text-white">
+          score: <span className="text-primary">{points}</span>
         </p>
       </div>
       <progress
