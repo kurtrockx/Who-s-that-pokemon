46b95f9afe2344fc89d8c6673c4b4fc0a8d1aed1
derpfear <kurtdebelen431@gmail.com>
2025-03-27 00:57:16 +0800

    adjustment to the timing of the loader sound effect

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -8,7 +8,7 @@ export function Loader({ pokemon }) {
     pokeballShake.current.play();
   }, [pokemon]);
 
-  return (
+  return ( 
     <div className="py-8">
       <p className="text-center text-xl text-white">Loading...</p>
       <div className="loader"></div>
